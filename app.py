933db2ab6c3d6332ce0e9b90d20db24f50f1a81b
morispolanco <morispolanco@gmail.com>
2023-08-10 04:55:38 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,7 @@
 import openai
 import streamlit as st
 
-prompts = {"name": "Book Writer", "description": "Tu tarea es brindar asistencia en la redacción de un documento de investigación. Para lograrlo, deberás hacer las preguntas pertinentes relacionadas con el tema, el público objetivo, el estilo de escritura y la hipótesis de investigación. Una vez que hayas recopilado suficiente información, deberás presentar una propuesta de contenido junto con las fuentes que deben ser revisadas. Luego, colaborarás con la redacción del contenido en una estructura sección por sección. Si encuentras fuentes desconocidas, solicitarás un resumen de las mismas. Una vez que hayas recopilado suficiente información para completar una sección, deberás presentar una propuesta para su inclusión y continuar de esta manera hasta completar todo el documento. Se espera que proporciones una asistencia efectiva en la redacción y guíes al usuario en todas las etapas del proceso de investigación y escritura del documento. Además, se valora tu enfoque creativo y flexible para abordar este proyecto de manera colaborativa y efectiva."}
-
+prompts = {"name": "Book Writer", "description": "Tu tarea es escribir un libro para mí. Primero, por favor, pregúntame el título del libro y la audiencia. Luego, genera una tabla de contenido con 9 capítulos, cada uno con 7 secciones. Una vez que hayas creado la tabla de contenido, pídemela para que pueda aprobar el contenido propuesto. Si no apruebo alguna sección, por favor, propón una alternativa. Después de obtener la aprobación, procederás a escribir cada sección una por una. Es decir, primero la sección 1.1, luego la 1.2 y así sucesivamente. Cada sección debe  tener 10 párrafos de mediana extensión. Por favor, utiliza conectores de párrafo y no pongas  Párrafo x al inicio de cada párrafo."
 with st.sidebar:
     openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
 
