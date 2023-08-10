263ad20e8ab9f5f36d649cfc4773c9b7d0075b98
morispolanco <morispolanco@gmail.com>
2023-08-10 02:23:55 -0600

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,22 @@ with st.sidebar:
 
 st.title("💬 Chatbot")
 if "messages" not in st.session_state:
-    st.session_state["messages"] = [{"role": "writer", "content": "How can I help you?"}]
+    st.session_state["messages"] = [{"role": "writer", "content": "Tu tarea es escribir un libro para mí. Primero, por favor, pregúntame "
+                                                                 "el título del libro y la audiencia. Luego, genera una tabla de "
+                                                                 "contenido con 9 capítulos, cada uno con 7 secciones. Una vez que "
+                                                                 "hayas creado la tabla de contenido, pídemela para que pueda aprobar "
+                                                                 "el contenido propuesto. Si no apruebo alguna sección, por favor, "
+                                                                 "propón una alternativa. Después de obtener la aprobación, procederás "
+                                                                 "a escribir cada sección una por una. Es decir, primero la sección 1.1, "
+                                                                 "luego la 1.2 y así sucesivamente. Cada sección debe tener 10 párrafos "
+                                                                 "de mediana extensión. Por favor, utiliza conectores de párrafo y no "
+                                                                 "pongas \"Párrafo x:\" al inicio de cada párrafo. \n"
+                                                                 
+                                                                 "Ten en cuenta que el contenido del libro debe ser coherente y relevante "
+                                                                 "para el título y los requisitos que te proporcione. Cuida el nivel. Si "
+                                                                 "te digo que es para un público culto, no escribas para estudiantes. También "
+                                                                 "es importante que respetes el formato de 7 secciones por capítulo y 10 "
+                                                                 "párrafos medianos por sección."}]
 
 for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
