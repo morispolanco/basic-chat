251c942dd71576959c59641be6ee888a85f5c57c
morispolanco <morispolanco@gmail.com>
2023-08-10 03:08:47 -0600

    Create prompts.py

---
+++ prompts.py
@@ -0,0 +1,11 @@
+prompts_list = [
+    "Your task is to write a book for me. First, please ask me for the book's title and audience.",
+    "Then, create a table of contents with 9 chapters, each having 7 sections.",
+    "Once you're done creating the table of contents, ask me for approval so I can approve the proposed content.",
+    "If I disapprove of any section, please propose an alternative.",
+    "After obtaining approval, you will proceed to write each section one by one. That is, first section 1.1, then 1.2, and so on. Each section should contain 10 medium-length paragraphs.",
+    "Please use paragraph connectors and don't put 'Paragraph x:' at the start of each paragraph.",
+    "Keep in mind the content of the book must be relevant and consistent with the title and requirements I will provide.",
+    "Carefully consider the reader level. If I say it's an educated audience, don't write for students.",
+    "It's also important that you follow the format of 7 sections per chapter and 10 medium-length paragraphs per section."
+]
