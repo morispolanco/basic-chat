4da2a0ca70b82dc5a42d25289f700d3c618f1032
morispolanco <morispolanco@gmail.com>
2023-08-10 03:13:49 -0600

    Update prompts.py

--- prompts.py
+++ prompts.py
@@ -1,11 +1,6 @@
-prompts_list = [
-    "Your task is to write a book for me. First, please ask me for the book's title and audience.",
-    "Then, create a table of contents with 9 chapters, each having 7 sections.",
-    "Once you're done creating the table of contents, ask me for approval so I can approve the proposed content.",
-    "If I disapprove of any section, please propose an alternative.",
-    "After obtaining approval, you will proceed to write each section one by one. That is, first section 1.1, then 1.2, and so on. Each section should contain 10 medium-length paragraphs.",
-    "Please use paragraph connectors and don't put 'Paragraph x:' at the start of each paragraph.",
-    "Keep in mind the content of the book must be relevant and consistent with the title and requirements I will provide.",
-    "Carefully consider the reader level. If I say it's an educated audience, don't write for students.",
-    "It's also important that you follow the format of 7 sections per chapter and 10 medium-length paragraphs per section."
+prompts = [
+    {"name": "Escritor de libros", 
+     "description": "Tu tarea es escribir un libro para mí. Primero, por favor, pregúntame el título del libro y la audiencia. Luego, genera una tabla de contenido con 9 capítulos, cada uno con 7 secciones. Una vez que hayas creado la tabla de contenido, pídemela para que pueda aprobar el contenido propuesto. Si no apruebo alguna sección, por favor, propón una alternativa. Después de obtener la aprobación, procederás a escribir cada sección una por una. Es decir, primero la sección 1.1, luego la 1.2 y así sucesivamente. Cada sección debe  tener 10 párrafos de mediana extensión. Por favor, utiliza conectores de párrafo y no pongas "Párrafo x:" al inicio de cada párrafo. Ten en cuenta que el contenido del libro debe ser coherente y relevante para el título y los requisitos que te proporcione. Cuida el nivel. Si te digo que es para un público culto, no escribas para estudiantes. También es importante que respetes el formato de 7 secciones por capítulo y 10 párrafos medianos por sección."
+    },
+    # Agrega más prompts aquí
 ]
