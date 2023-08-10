3c7b30ff9b3e2a8aab8a2e1ffefe90ea74efa71f
morispolanco <morispolanco@gmail.com>
2023-08-10 04:49:23 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import openai
 import streamlit as st
 
-prompts = {"name": "Book Writer", "description": "Your task is to write a book..."}
+prompts = {"name": "Book Writer", "description": "Tu tarea es brindar asistencia en la redacción de un documento de investigación. Para lograrlo, deberás hacer las preguntas pertinentes relacionadas con el tema, el público objetivo, el estilo de escritura y la hipótesis de investigación. Una vez que hayas recopilado suficiente información, deberás presentar una propuesta de contenido junto con las fuentes que deben ser revisadas. Luego, colaborarás con la redacción del contenido en una estructura sección por sección. Si encuentras fuentes desconocidas, solicitarás un resumen de las mismas. Una vez que hayas recopilado suficiente información para completar una sección, deberás presentar una propuesta para su inclusión y continuar de esta manera hasta completar todo el documento. Se espera que proporciones una asistencia efectiva en la redacción y guíes al usuario en todas las etapas del proceso de investigación y escritura del documento. Además, se valora tu enfoque creativo y flexible para abordar este proyecto de manera colaborativa y efectiva."}
 
 with st.sidebar:
     openai_api_key = st.text_input("OpenAI API Key", key="chatbot_api_key", type="password")
